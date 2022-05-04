bdb3086cb11c0bad33d4b4bb62a53b4417052bc6
akozyrev <lexxko@ya.ru>
2022-05-04 17:30:56 +0200

    Update 7

--- src/main/java/org/my/controller/Controller.java
+++ src/main/java/org/my/controller/Controller.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 @RestController()
 public class Controller {
     private final FeedService feedService;
+
     @Autowired
     public Controller(FeedService feedService) {
         this.feedService = feedService;
@@ -28,5 +31,19 @@ public class Controller {
     @GetMapping(path = "feedStop")
     public Mono<String> feedStop() {
         return feedService.feedStop();
+    }
+
+    @GetMapping(path = "feedDisplay")
+    public Flux<String> feedDisplay() {
+        return feedService.feedDisplay()
+                .map(Controller::createHyperlink);
+    }
+
+    private static String createHyperlink(Tuple2<String, String> tuple) {
+        if (tuple.getT1().contains("No data") || tuple.getT2().contains("No data")) {
+            return "No data";
+        }
+
+        return String.format("<a href=%s>%s</a><br>", tuple.getT1(), tuple.getT2());
     }
 }
--- src/main/java/org/my/service/FeedService.java
+++ src/main/java/org/my/service/FeedService.java
@@ -11,14 +11,17 @@ import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.annotation.Nullable;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class FeedService {
     public static final Logger log = LoggerFactory.getLogger(FeedService.class);
     private final FluxMessageChannel channel;
     private @Nullable Disposable disposable = null;
+    private final List<Tuple2<String, String>> feed = new ArrayList<>();
 
     @Autowired
     public FeedService(FluxMessageChannel channel) {
@@ -39,13 +42,12 @@ public class FeedService {
                     .filter(Objects::nonNull)
                     .map(msg -> (SyndEntryImpl) msg)
                     .filter(msg -> !msg.getTitle().isEmpty() && !msg.getLink().isEmpty())
-                    .doOnNext(msg -> log.info(msg.getTitle() + " " + removeUrlParams(msg.getLink())))
+                    .doOnNext(this::processMessage)
                     .doOnError(ClassCastException.class, e -> log.error("Class cast error: " + e.getMessage()))
                     .doOnError(err -> log.error(err.toString()))
                     .doOnComplete(() -> log.info("Feeding completed!"))
                     .subscribe();
         }
-
         return Mono.just("Feeding started!");
     }
 
@@ -61,6 +63,21 @@ public class FeedService {
             log.info("Feeding stopped!");
         }
         return Mono.just("Feeding stopped!");
+    }
+
+    public Flux<Tuple2<String, String>> feedDisplay() {
+        if (feed.isEmpty()) {
+            return Flux.just(Tuples.of("No data", "No data"));
+        }
+
+        return Flux.fromIterable(feed);
+    }
+
+    private void processMessage(SyndEntryImpl msg) {
+        final String link = removeUrlParams(msg.getLink());
+        final String title = msg.getTitle();
+        feed.add(Tuples.of(link, title));
+        log.info(title + " " + link);
     }
 
     private boolean isDisposed() {
